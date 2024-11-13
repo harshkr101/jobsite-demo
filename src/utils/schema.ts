efa27a9861c83babc3fc0be3411f18ee9eba1091
codehead <developer.harsh101@gmail.com>
2024-11-13 13:31:38 +0530

    adds login schema

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,33 +1,38 @@
-import { Request,Response,NextFunction } from 'express';
-import { AnyZodObject, z } from 'zod';
+import { Request, Response, NextFunction } from "express";
+import { AnyZodObject, z } from "zod";
 
 // validation schemas
 
 export const UserSchema = z.object({
-    firstName: z.string().max(100),
-    lastName: z.string().max(100),
-    email: z.string().email('Invalid email format'),
-    password: z.string().min(8, 'Password must contain at least 8 characters'),
-    type:z.string()
+  firstName: z.string().max(100),
+  lastName: z.string().max(100),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(8, "Password must contain at least 8 characters"),
+  type: z.string(),
 });
 
 export const jobSchema = z.object({
-    title: z.string().max(100),
-    description: z.string().max(500),
-    status: z.string()
+  title: z.string().max(100),
+  description: z.string().max(500),
+  status: z.string(),
 });
 
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
 
 // validate function
 
-export const validate = (schema: AnyZodObject) =>
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        await schema.parseAsync({
-          ...req.body,
-        });
-        return next();
-      } catch (error) {
-        return res.status(400).json(error);
-      }
+export const validate =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        ...req.body,
+      });
+      return next();
+    } catch (error) {
+      return res.status(400).json(error);
+    }
   };
